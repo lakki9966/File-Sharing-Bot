5b08cd2901410403b283a309cb8adde83391d9a0
lakki9966 <duduffccuf@gmail.com>
2025-05-31 05:52:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
-#main.py
+from pyrogram import Client
+from config import Config
+from handlers import commands, files, batch
 
-from pyrogram import Client from config import Config from handlers import commands, files, batch
+app = Client(
+    "file-store-bot",
+    api_id=Config.API_ID,
+    api_hash=Config.API_HASH,
+    bot_token=Config.BOT_TOKEN
+)
 
-app = Client( "file-store-bot", api_id=Config.API_ID, api_hash=Config.API_HASH, bot_token=Config.BOT_TOKEN )
+# Register Handlers
+commands.register(app)
+files.register(app)
+batch.register(app)
 
-Register Handlers
-
-commands.register(app) files.register(app) batch.register(app)
-
-print("Bot Started") app.run()
-
+print("Lakki Started")
+app.run()
