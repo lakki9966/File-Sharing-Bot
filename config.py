6ccdbe6031a11064141099764dc00acd5348c575
lakki9966 <duduffccuf@gmail.com>
2025-05-31 20:19:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-# config.py
+# ========== config.py ==========
 import os
 
 class Config:
@@ -6,5 +6,7 @@ class Config:
     API_HASH = os.environ.get("API_HASH")
     BOT_TOKEN = os.environ.get("BOT_TOKEN")
     DB_URI = os.environ.get("DB_URI")  # MongoDB URI
-    VERIFICATION_CHANNEL = int(os.environ.get("VERIFICATION_CHANNEL", 0))
+    DB_CHANNEL_ID = int(os.environ.get("DB_CHANNEL_ID", -100123456789))  # Permanent storage
     ADMIN_IDS = [int(id) for id in os.environ.get("ADMIN_IDS", "").split()]
+    VERIFICATION_CHANNEL = int(os.environ.get("VERIFICATION_CHANNEL", -100789654321))  # Optional
+    DEFAULT_EXPIRY = int(os.environ.get("DEFAULT_EXPIRY", 1800))  # 30 mins in seconds
