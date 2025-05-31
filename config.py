e44f4815ba35bdc8e38056fa0bcb137774f72ae6
lakki9966 <duduffccuf@gmail.com>
2025-05-31 19:36:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,10 @@
+# config.py
 import os
 
 class Config:
-    API_ID = int(os.getenv("API_ID"))
-    API_HASH = os.getenv("API_HASH")
-    BOT_TOKEN = os.getenv("BOT_TOKEN")
-    MONGO_URI = os.getenv("MONGO_URI")
-    DATABASE_CHANNEL = int(os.getenv("DATABASE_CHANNEL"))
-    ADMIN_IDS = list(map(int, os.getenv("ADMIN_IDS", "").split()))
-    ALLOWED_USERS = list(map(int, os.getenv("ALLOWED_USERS", "").split()))
-    DEFAULT_EXPIRY = int(os.getenv("DEFAULT_EXPIRY", 1800))  # 30 minutes
+    API_ID = int(os.environ.get("API_ID"))
+    API_HASH = os.environ.get("API_HASH")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN")
+    DB_URI = os.environ.get("DB_URI")  # MongoDB URI
+    VERIFICATION_CHANNEL = int(os.environ.get("VERIFICATION_CHANNEL", 0))
+    ADMIN_IDS = [int(id) for id in os.environ.get("ADMIN_IDS", "").split()]
