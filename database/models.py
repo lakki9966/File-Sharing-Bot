b114887145fa8b29be0c0d6407c9a44c140f9f2a
lakki9966 <duduffccuf@gmail.com>
2025-06-01 06:13:48 +0530

    Update models.py

--- database/models.py
+++ database/models.py
@@ -3,15 +3,28 @@ from pymongo import MongoClient
 from config import Config
 
 client = MongoClient(Config.DB_URI)
-db = client["FileShareBot"]
+db = client["FileStorageBot"]
 
 class File:
     def __init__(self):
         self.collection = db["files"]
     
     def add_file(self, file_data):
-        return self.collection.insert_one({
-            **file_data,
-            "created_at": datetime.now(),
-            "expiry_status": "active"
-        })
+        """Insert file metadata with timestamp"""
+        file_data["created_at"] = datetime.now()
+        return self.collection.insert_one(file_data)
+
+class User:
+    def __init__(self):
+        self.collection = db["users"]
+    
+    def add_user(self, user_id, username=None):
+        """Upsert user data"""
+        return self.collection.update_one(
+            {"user_id": user_id},
+            {"$set": {
+                "username": username,
+                "last_active": datetime.now()
+            }},
+            upsert=True
+        )
