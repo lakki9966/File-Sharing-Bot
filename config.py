6ff85b00e132c6b72f01bd4af308cedc3d2af43b
lakki9966 <duduffccuf@gmail.com>
2025-06-01 09:00:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,13 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Config:
-    # Telegram API (Must have)
-    API_ID = int(os.environ.get("API_ID"))  # my.telegram.org
-    API_HASH = os.environ.get("API_HASH")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")  # From @BotFather
-    
-    # Database (Must have)
-    DB_URI = os.environ.get("DB_URI")  # MongoDB URL
-    DB_CHANNEL_ID = int(os.environ.get("DB_CHANNEL_ID", -1001234567890))  # Your private channel ID
-    
-    # Admin System
-    OWNER_ID = int(os.environ.get("OWNER_ID", 123456789))  # Your Telegram ID here
+    API_ID = int(os.getenv("API_ID"))
+    API_HASH = os.getenv("API_HASH")
+    BOT_TOKEN = os.getenv("BOT_TOKEN")
+    DB_URI = os.getenv("DB_URI")
+    DB_CHANNEL_ID = int(os.getenv("DB_CHANNEL_ID"))
+    OWNER_ID = int(os.getenv("OWNER_ID"))
+    DEFAULT_EXPIRY = 60 * 60  # 1 hour
