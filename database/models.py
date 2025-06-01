c0deb67cedf578aa542adb171e476967178adf72
lakki9966 <duduffccuf@gmail.com>
2025-06-01 06:59:14 +0530

    Update models.py

--- database/models.py
+++ database/models.py
@@ -2,19 +2,10 @@ from datetime import datetime
 from pymongo import MongoClient
 from config import Config
 
-class Database:
-    def __init__(self):
-        self.client = MongoClient(Config.DB_URI)
-        self.db = self.client["FileShareDB"]
-    
-    class File:
-        def __init__(self, db):
-            self.collection = db["files"]
-        
-        def add_file(self, data):
-            data["created_at"] = datetime.now()
-            return self.collection.insert_one(data)
+client = MongoClient(Config.DB_URI)
+db = client["FileShareDB"]
 
-# Initialize
-db = Database()
-File = db.File(db.db)
+class File:
+    collection = db["files"]  # This is now a class variable
+
+# No need for instantiation
