c27d5612a6dd51f7e9aaa39ffb245c94231950ee
lakki9966 <duduffccuf@gmail.com>
2025-06-01 06:14:56 +0530

    Update shortlink.py

--- utils/shortlink.py
+++ utils/shortlink.py
@@ -2,5 +2,6 @@ import secrets
 import string
 
 def generate_shortcode(length=6):
-    """Generate random shortcode (e.g. zSy1WV)"""
-    return ''.join(secrets.choice(string.ascii_letters + string.digits) for _ in range(length))
+    """Generate random case-sensitive shortcode"""
+    chars = string.ascii_letters + string.digits  # A-Z, a-z, 0-9
+    return ''.join(secrets.choice(chars) for _ in range(length))
