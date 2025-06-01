df462a7009198074d425e47664b2914e74e5a884
lakki9966 <duduffccuf@gmail.com>
2025-06-01 05:54:28 +0530

    Update shortlink.py

--- utils/shortlink.py
+++ utils/shortlink.py
@@ -1,6 +1,6 @@
-# ========== utils/shortlink.py ==========
 import secrets
 import string
 
 def generate_shortcode(length=6):
+    """Generate random shortcode (e.g. zSy1WV)"""
     return ''.join(secrets.choice(string.ascii_letters + string.digits) for _ in range(length))
