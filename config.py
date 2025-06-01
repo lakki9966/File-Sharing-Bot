0a2969c054b15e5c913cd7a23d76ec8d608a9168
lakki9966 <duduffccuf@gmail.com>
2025-06-01 07:28:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,14 @@
 import os
 
 class Config:
-    # Must have values (Ivi pettali)
-    API_ID = int(os.environ.get("API_ID"))  # Telegram API ID (my.telegram.org)
-    API_HASH = os.environ.get("API_HASH")  # Telegram API Hash
+    # Telegram API (Must have)
+    API_ID = int(os.environ.get("API_ID"))  # my.telegram.org
+    API_HASH = os.environ.get("API_HASH")
     BOT_TOKEN = os.environ.get("BOT_TOKEN")  # From @BotFather
-    DB_CHANNEL_ID = int(os.environ.get("DB_CHANNEL_ID"))  # Private channel ID (-100...)
+    
+    # Database (Must have)
+    DB_URI = os.environ.get("DB_URI")  # MongoDB URL
+    DB_CHANNEL_ID = int(os.environ.get("DB_CHANNEL_ID", -1001234567890))  # Your private channel ID
+    
+    # Optional
+    ADMIN_IDS = [int(id) for id in os.environ.get("ADMIN_IDS", "").split()]
