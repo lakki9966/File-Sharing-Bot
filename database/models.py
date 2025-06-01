572b1a917847a465579eb8799ccd3ea30102d202
lakki9966 <duduffccuf@gmail.com>
2025-06-01 08:47:15 +0530

    Update models.py

--- database/models.py
+++ database/models.py
@@ -16,29 +16,9 @@ class File:
     @classmethod
     def get_user_files(cls, user_id):
         return list(cls.collection.find({"uploader_id": user_id}))
+    
+    @classmethod
+    def get_by_random_id(cls, random_id):
+        return cls.collection.find_one({"random_id": random_id})
 
-class User:
-    collection = db["users"]
-    
-    @classmethod
-    def add_user(cls, user_id, username=None):
-        return cls.collection.update_one(
-            {"user_id": user_id},
-            {"$set": {"username": username, "last_seen": datetime.now()}},
-            upsert=True
-        )
-
-class Admin:
-    collection = db["admins"]
-    
-    @classmethod
-    def add_admin(cls, user_id):
-        return cls.collection.update_one(
-            {"user_id": user_id},
-            {"$set": {"added_at": datetime.now()}},
-            upsert=True
-        )
-    
-    @classmethod
-    def is_admin(cls, user_id):
-        return cls.collection.find_one({"user_id": user_id}) is not None
+# [Keep User and Admin classes exactly the same]
