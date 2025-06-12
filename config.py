382eb9bcbc8394c4a1f09c69955fbcfe71431c88
lakki9966 <duduffccuf@gmail.com>
2025-06-12 17:51:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,24 @@
+# config.py
+
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
+API_ID = int(os.getenv("API_ID", "123456"))
+API_HASH = os.getenv("API_HASH", "your_api_hash")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "your_bot_token")
 
-class Config:
-    API_ID = int(os.getenv("API_ID"))
-    API_HASH = os.getenv("API_HASH")
-    BOT_TOKEN = os.getenv("BOT_TOKEN")
-    DB_URI = os.getenv("DB_URI")
-    DB_CHANNEL_ID = int(os.getenv("DB_CHANNEL_ID"))
-    OWNER_ID = int(os.getenv("OWNER_ID"))
-    DEFAULT_EXPIRY = 60 * 60  # 1 hour
+# MongoDB
+MONGO_URI = os.getenv("MONGO_URI", "your_mongo_uri")
+DB_NAME = "file_share_db"
+
+# DB channel ID
+DB_CHANNEL = int(os.getenv("DB_CHANNEL", "-100XXXXXXXXXX"))
+
+# Expiry time in minutes for PM file delete
+DEFAULT_EXPIRY = 30
+
+# Admins and Owner
+OWNER_ID = int(os.getenv("OWNER_ID", "123456789"))
+ADMINS = [OWNER_ID]  # can be updated dynamically
+
+# Bot Username (used in link shortener)
+BOT_USERNAME = os.getenv("BOT_USERNAME", "YourBotUsername")
