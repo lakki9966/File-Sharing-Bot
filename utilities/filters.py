ef6f9d9ea21ea5a4ddc47ea170d75a23105d2fbe
lakki9966 <duduffccuf@gmail.com>
2025-06-12 18:12:58 +0530

    Delete utilities/filters.py

--- utilities/filters.py
+++
@@ -1,9 +0,0 @@
-from pyrogram import filters
-from database.models import Admin
-
-def admin_filter(_, __, message):
-    if not message.from_user:
-        return False
-    return Admin.is_admin(message.from_user.id)
-
-admin_only = filters.create(admin_filter)
