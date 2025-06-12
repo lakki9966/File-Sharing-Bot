7d07dfc145fed792f16b0768242b9a4eabd1c90a
lakki9966 <duduffccuf@gmail.com>
2025-06-12 19:10:07 +0530

    Update shortener.py

--- utilities/shortener.py
+++ utilities/shortener.py
@@ -3,5 +3,6 @@
 import random
 import string
 
-def generate_shortlink(length=7):
-    return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
+def generate_shortlink(length=8):
+    characters = string.ascii_letters + string.digits
+    return ''.join(random.choices(characters, k=length))
