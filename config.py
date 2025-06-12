5214b45cc15b2850ade0be8464be03741f4bb3f2
lakki9966 <duduffccuf@gmail.com>
2025-06-12 21:28:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,23 +2,23 @@
 
 import os
 
-API_ID = int(os.getenv("API_ID", "123456"))
-API_HASH = os.getenv("API_HASH", "your_api_hash")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "your_bot_token")
+API_ID = int(os.getenv("API_ID", "22432833"))
+API_HASH = os.getenv("API_HASH", "897f1c440892cfc46c7e222dfb37d015")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7746298562:AAEF2LmH02RM-GnbyOiF3_Ws2CLDJBac6iA")
 
 # MongoDB
-MONGO_URI = os.getenv("MONGO_URI", "your_mongo_uri")
+MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://eno2223456:7Cdmqig5Ih2vrqW4@cluster0.ccpmee5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DB_NAME = "file_share_db"
 
 # DB channel ID
-DB_CHANNEL = int(os.getenv("DB_CHANNEL", "-100XXXXXXXXXX"))
+DB_CHANNEL = int(os.getenv("DB_CHANNEL", "-1002609374874"))
 
 # Expiry time in minutes for PM file delete
 DEFAULT_EXPIRY = 30
 
 # Admins and Owner
-OWNER_ID = int(os.getenv("OWNER_ID", "123456789"))
+OWNER_ID = int(os.getenv("OWNER_ID", "7592041488"))
 ADMINS = [OWNER_ID]  # can be updated dynamically
 
 # Bot Username (used in link shortener)
-BOT_USERNAME = os.getenv("BOT_USERNAME", "YourBotUsername")
+BOT_USERNAME = os.getenv("BOT_USERNAME", "Gpt_test_file_sharebot")
