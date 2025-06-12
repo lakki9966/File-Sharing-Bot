256f7a8aa1ec1a807b5ef67c09fc7a1f65b69632
lakki9966 <duduffccuf@gmail.com>
2025-06-12 17:55:51 +0530

    Create mongodb.py

---
+++ database/mongodb.py
@@ -0,0 +1,12 @@
+# database/mongodb.py
+
+from pymongo import MongoClient
+from config import MONGO_URI, DB_NAME
+
+client = MongoClient(MONGO_URI)
+db = client[DB_NAME]
+
+files_col = db.files
+session_col = db.user_sessions
+users_col = db.users
+admins_col = db.admins
