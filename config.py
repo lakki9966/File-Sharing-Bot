59d45420a0acde32869dfff4f193623f9d01b3c9
lakki9966 <duduffccuf@gmail.com>
2025-06-13 05:52:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 
 API_ID = int(os.getenv("API_ID", "22432833"))
 API_HASH = os.getenv("API_HASH", "897f1c440892cfc46c7e222dfb37d015")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "7746298562:AAEF2LmH02RM-GnbyOiF3_Ws2CLDJBac6iA")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7746298562:AAHb1WG_k3LS8ffCA8n3AJnnSUAa9li2-4U")
 
 # MongoDB
 MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://eno2223456:7Cdmqig5Ih2vrqW4@cluster0.ccpmee5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
